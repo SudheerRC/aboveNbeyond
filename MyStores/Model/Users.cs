534847352f09c19ef24d7e6052796bd9fe10912e
Sudheer Chintalapudi <sc00308@my.westga.edu>
2023-06-13 21:44:25 -0400

    Modified Users Class

--- MyStores/Model/Users.cs
+++ MyStores/Model/Users.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MyStores.Model
+﻿namespace MyStores.Model
 {
-    internal class Users
+    public class Users
     {
+        public int UserId { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string DOB { get; set; }
+
+        public string Gender { get; set; }
+
+        public string StreetAddress { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public string Country { get; set; }
+
     }
 }
