8bbe25b447091075a7e11b0f3e8ef5d6980fedb2
vk00019 <vk00019@my.westga.edu>
2023-07-03 21:14:16 -0400

    Added funnctionality for order user control

--- MyStores/UserControls/OrderUserControl.cs
+++ MyStores/UserControls/OrderUserControl.cs
@@ -41,18 +41,33 @@ namespace MyStores.UserControls
             listPanelComboBox.SelectedIndex.Equals(0);
         }
 
-        private void placeOrderButton_Click(object sender, EventArgs e)
+        private void LoadProductList()
         {
-            finalListPanelButton.Text = @"Place Order";
-            panelHeadingLabel.Text = @"Please select a Vendor from this list to place an order";
+            var vendor = listPanelComboBox.SelectedItem as Vendor;
+            List<InventoryItem> itemList = _controller.SearchInventoryWithVendorId(vendor.Id, _storeId);
 
-            SwitchPanels();
+            mainFlowLayoutPanel.Controls.Clear();
+
+            foreach (var t in itemList)
+            {
+                var productTile = new OrderedProductTileUserControl();
+                productTile.setInventoryItem(t);
+                mainFlowLayoutPanel.Controls.Add(productTile);
+            }
         }
 
         private void SwitchPanels()
         {
             mainPanel.Visible = false;
             listPanel.Visible = true;
+        }
+
+        private void placeOrderButton_Click(object sender, EventArgs e)
+        {
+            finalListPanelButton.Text = @"Place Order";
+            panelHeadingLabel.Text = @"Please select a Vendor from this list to place an order";
+
+            SwitchPanels();
         }
 
         private void receiveOrderButton_Click(object sender, EventArgs e)
@@ -65,7 +80,10 @@ namespace MyStores.UserControls
 
         private void backButton_Click(object sender, EventArgs e)
         {
+            mainPanel.Visible = true;
+            listPanel.Visible = false;
 
+            mainFlowLayoutPanel.Controls.Clear();
         }
 
         private void finalListPanelButton_Click(object sender, EventArgs e)
