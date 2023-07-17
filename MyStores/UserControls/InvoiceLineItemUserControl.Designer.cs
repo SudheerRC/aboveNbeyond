3d2553033eece2891bff3892c6ed5038ca94c751
Sudheer Chintalapudi <sc00308@my.westga.edu>
2023-07-17 13:38:29 -0400

    Added Invoice line item user control and its design

---
+++ MyStores/UserControls/InvoiceLineItemUserControl.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace MyStores.UserControls
+{
+    partial class InvoiceLineItemUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            productNameLabel = new Label();
+            discountHeadingabel = new Label();
+            discountValueLabel = new Label();
+            label3 = new Label();
+            quantityLabel = new Label();
+            unitPriceLabel = new Label();
+            sizeLabel = new Label();
+            quantityHeadingLabel = new Label();
+            unitPriceHeadingLabel = new Label();
+            productIdLabel = new Label();
+            dividerPanel = new Panel();
+            SuspendLayout();
+            // 
+            // productNameLabel
+            // 
+            productNameLabel.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
+            productNameLabel.Location = new Point(3, 0);
+            productNameLabel.Name = "productNameLabel";
+            productNameLabel.Size = new Size(354, 36);
+            productNameLabel.TabIndex = 2;
+            productNameLabel.Text = "Product Name";
+            productNameLabel.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // discountHeadingabel
+            // 
+            discountHeadingabel.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
+            discountHeadingabel.ForeColor = Color.Red;
+            discountHeadingabel.Location = new Point(172, 37);
+            discountHeadingabel.Name = "discountHeadingabel";
+            discountHeadingabel.Size = new Size(92, 35);
+            discountHeadingabel.TabIndex = 3;
+            discountHeadingabel.Text = "Discount:";
+            discountHeadingabel.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // discountValueLabel
+            // 
+            discountValueLabel.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
+            discountValueLabel.ForeColor = Color.Red;
+            discountValueLabel.Location = new Point(270, 37);
+            discountValueLabel.Name = "discountValueLabel";
+            discountValueLabel.Size = new Size(87, 35);
+            discountValueLabel.TabIndex = 4;
+            discountValueLabel.Text = "$ 1.99";
+            discountValueLabel.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            label3.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(363, 37);
+            label3.Name = "label3";
+            label3.Size = new Size(76, 35);
+            label3.TabIndex = 5;
+            label3.Text = "20Oz";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // quantityLabel
+            // 
+            quantityLabel.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
+            quantityLabel.Location = new Point(445, 37);
+            quantityLabel.Name = "quantityLabel";
+            quantityLabel.Size = new Size(79, 35);
+            quantityLabel.TabIndex = 6;
+            quantityLabel.Text = "8";
+            quantityLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // unitPriceLabel
+            // 
+            unitPriceLabel.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
+            unitPriceLabel.Location = new Point(530, 37);
+            unitPriceLabel.Name = "unitPriceLabel";
+            unitPriceLabel.Size = new Size(89, 35);
+            unitPriceLabel.TabIndex = 7;
+            unitPriceLabel.Text = "$ 2.99";
+            unitPriceLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // sizeLabel
+            // 
+            sizeLabel.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            sizeLabel.Location = new Point(363, 0);
+            sizeLabel.Name = "sizeLabel";
+            sizeLabel.Size = new Size(76, 36);
+            sizeLabel.TabIndex = 8;
+            sizeLabel.Text = "Size";
+            sizeLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // quantityHeadingLabel
+            // 
+            quantityHeadingLabel.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            quantityHeadingLabel.Location = new Point(445, 0);
+            quantityHeadingLabel.Name = "quantityHeadingLabel";
+            quantityHeadingLabel.Size = new Size(79, 36);
+            quantityHeadingLabel.TabIndex = 9;
+            quantityHeadingLabel.Text = "Quantity";
+            quantityHeadingLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // unitPriceHeadingLabel
+            // 
+            unitPriceHeadingLabel.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            unitPriceHeadingLabel.Location = new Point(530, 0);
+            unitPriceHeadingLabel.Name = "unitPriceHeadingLabel";
+            unitPriceHeadingLabel.Size = new Size(89, 36);
+            unitPriceHeadingLabel.TabIndex = 10;
+            unitPriceHeadingLabel.Text = "Unit Price";
+            unitPriceHeadingLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // productIdLabel
+            // 
+            productIdLabel.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            productIdLabel.Location = new Point(3, 37);
+            productIdLabel.Name = "productIdLabel";
+            productIdLabel.Size = new Size(163, 36);
+            productIdLabel.TabIndex = 11;
+            productIdLabel.Text = "#22222";
+            productIdLabel.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // dividerPanel
+            // 
+            dividerPanel.BackColor = Color.FromArgb(28, 44, 78);
+            dividerPanel.Location = new Point(0, 76);
+            dividerPanel.Name = "dividerPanel";
+            dividerPanel.Size = new Size(622, 10);
+            dividerPanel.TabIndex = 12;
+            // 
+            // InvoiceLineItemUserControl
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(227, 211, 177);
+            Controls.Add(dividerPanel);
+            Controls.Add(productIdLabel);
+            Controls.Add(unitPriceHeadingLabel);
+            Controls.Add(quantityHeadingLabel);
+            Controls.Add(sizeLabel);
+            Controls.Add(unitPriceLabel);
+            Controls.Add(quantityLabel);
+            Controls.Add(label3);
+            Controls.Add(discountValueLabel);
+            Controls.Add(discountHeadingabel);
+            Controls.Add(productNameLabel);
+            Name = "InvoiceLineItemUserControl";
+            Size = new Size(622, 85);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label productNameLabel;
+        private Label discountHeadingabel;
+        private Label discountValueLabel;
+        private Label label3;
+        private Label quantityLabel;
+        private Label unitPriceLabel;
+        private Label sizeLabel;
+        private Label quantityHeadingLabel;
+        private Label unitPriceHeadingLabel;
+        private Label productIdLabel;
+        private Panel dividerPanel;
+    }
+}
